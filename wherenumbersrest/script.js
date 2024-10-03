10ea9a06aeb5e9dbdf4ed52a327c668c4edf271c
liuj012 <145478562+liuj012@users.noreply.github.com>
2024-10-03 09:10:33 -0400

    updated

--- wherenumbersrest/script.js
+++ wherenumbersrest/script.js
@@ -2,18 +2,22 @@ $(document).ready(function() {
     $('.letter').click(function() {
         let letterId = $(this).attr('id');
 
-        let documentWidth = $(document).width();
-        let documentHeight = $(document).height();
+        let windowWidth = $(window).width();
+        let windowHeight = $(window).height();
 
         let scrollTop = $(window).scrollTop();
         let scrollLeft = $(window).scrollLeft();
 
         $('.img' + letterId).each(function() {
-            let randomTop = Math.floor(Math.random() * (documentHeight - $(this).height())) + scrollTop;
-            let randomLeft = Math.floor(Math.random() * (documentWidth - $(this).width())) + scrollLeft;
+            let imgHeight = $(this).height();
+            let imgWidth = $(this).width();
+
+            let randomTop = Math.floor(Math.random() * (windowHeight - imgHeight)) + scrollTop;
+            let randomLeft = Math.floor(Math.random() * (windowWidth - imgWidth)) + scrollLeft;
 
             $(this).css({
                 display: 'block',
+                position: 'absolute',
                 top: randomTop + 'px',
                 left: randomLeft + 'px'
             });
